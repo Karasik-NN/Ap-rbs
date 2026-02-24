22b6dd5ea4469127c867d78801ca4f0dedcf15ac
Karasik-NN <nikitatt3914@gmail.com>
2026-02-24 19:29:15 +0200

    uztaisiju skaņu uz klik Main Menu

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,12 +3,20 @@ using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
-    }
+   public void StartGame()
+{
+    AudioManager.instance.PlayClick();
+    Invoke("LoadNextScene", 0.15f);
+}
+
+void LoadNextScene() => UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+
     public void QuitGame()
     {
+        if (AudioManager.instance != null)
+        {
+            AudioManager.instance.PlayClick();
+        }
         Application.Quit();
     }
 }
