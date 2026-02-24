753736c46a05659f78e219cd960e7409dd2aa318
Karasik-NN <nikitatt3914@gmail.com>
2026-02-24 15:28:29 +0200

    uzlaboju skriptu InventorySlot lai nevaru ielikt viena slota vairakus priekšmetus

--- Assets/Scripts/InventorySlot.cs
+++ Assets/Scripts/InventorySlot.cs
@@ -3,16 +3,31 @@ using UnityEngine.EventSystems;
 
 public class InventorySlot : MonoBehaviour, IDropHandler
 {
-    public virtual void OnDrop(PointerEventData eventData)
+    public string requiredType = ""; 
+
+    public void OnDrop(PointerEventData eventData)
     {
         if (eventData.pointerDrag != null)
         {
             DragItem item = eventData.pointerDrag.GetComponent<DragItem>();
+
             if (item != null)
             {
-                item.isSnapped = true;
-                item.transform.SetParent(transform);
-                item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                bool typeMatches = string.IsNullOrEmpty(requiredType) || item.itemType == requiredType;
+                
+                bool isEmpty = transform.childCount == 0;
+
+                if (typeMatches && isEmpty)
+                {
+                    item.isSnapped = true;
+                    item.transform.SetParent(transform);
+                    item.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+                    Debug.Log("Предмет успешно положен");
+                }
+                else
+                {
+                    Debug.Log("Нельзя положить: либо тип не тот, либо слот занят");
+                }
             }
         }
     }
